ad83eaf264bb63327610e49581bb9de1271d0e0f
Andrei Bashak <andrei.bashak@nxtec-corp.com>
2024-07-17 15:39:00 +0200

    rollback func wpf

--- research/FunctionalProgramming/src/Functional.Core.WPF/ColorTriplet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Functional.Core.WPF;
-// a simple color triplet representation
-
-public struct ColorTriplet
-{
-    public double X { get; init; }
-    public double Y { get; init; }
-    public double Z { get; init; }
-}
